b04b93f1a88a0fd7e4dbb52d1cfd7b75515d1514
Ammad Ali <73386704+Mr-Nobody1@users.noreply.github.com>
2022-07-28 20:15:52 +0500

    Completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,9 +19,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
     
-
-    res.render("home", {para: homeStartingContent , posts: posts});
-
+    res.render("home", {para: homeStartingContent , posts: posts });
 
 });  
 
@@ -44,23 +43,40 @@ app.post("/compose", function(req, res){
     title: req.body.postTitle,
     content: req.body.postBody
   };
+
+  
   posts.push(post);
-  
-  // res.send();
-  
    
   res.redirect("/");
 });
 
+app.get("/posts/:postName", function(req, res){
+  
+  posts.forEach(function(post){
+    const requestedTitle =_.lowerCase( req.params.postName);
+    const storedTitle =_.lowerCase( post.title);
+    if(storedTitle === requestedTitle){
+      
+      res.render("post", {title: post.title, content: post.content});
+      // res.redirect("post");
+    }
+  });
+ 
+}); 
 
 
-
-
-
-
-
-
-
+// const requestedTitle = req.params.postName;
+//   let found = false;
+//   posts.forEach(function(post){
+//     if(post.title === requestedTitle){
+//       found = true;
+//       console.log("match found");
+//     }
+//   });
+//   if(!found){
+//     console.log("no match found");
+//   }
+// }); 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
